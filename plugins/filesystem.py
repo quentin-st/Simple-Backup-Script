d43d1c3690f020780e4ca16b2c4d8ca61f70749f
Quentin Stoeckel <chteuchteu@gmail.com>
2015-10-23 21:59:19 +0200

    Filesystem: create separate tar.gz for each directory

--- plugins/filesystem.py
+++ plugins/filesystem.py
@@ -3,6 +3,7 @@ import os
 import shutil
 
 from utils import stdio
+from utils.stdio import CRESET, CBOLD, LGREEN
 
 
 def get_main_class():
@@ -21,15 +22,27 @@ class Filesystem:
         tmp_dir = stdio.simple_exec('mktemp',  '--directory')
         self.temp_dir = tmp_dir
 
-        # Create tar file
+        # cd to temp dir
         saved_path = os.getcwd()
         os.chdir(tmp_dir)
 
+        # Create tar file
         tar = tarfile.open('archive.tar.gz', 'w:gz')
 
         # Loop over directories
         for directory in backup.get('directories'):
-            tar.add(directory, arcname=os.path.basename(os.path.normpath(directory)))
+            dir_name = os.path.basename(os.path.normpath(directory))
+
+            # Create single tar.gz file for this dir
+            singletar_filename = dir_name + ".tar.gz"
+            print(CBOLD+LGREEN, "\nCreating " + singletar_filename, CRESET)
+
+            single_tar = tarfile.open(singletar_filename, 'w:gz')
+            single_tar.add(directory, arcname=dir_name)
+            single_tar.close()
+
+            # Add it to global tar file
+            tar.add(singletar_filename, arcname=dir_name)
 
         tar.close()
 
